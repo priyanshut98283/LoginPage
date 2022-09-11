07b6ffd5dadd215a0bde3848a2bed90066858c6b
Priyanshu Tiwari <priyanshut98283@gmail.com>
2022-09-11 11:33:42 +0530

    First

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+import Login from './Login';
+import Login2 from './Login2';
+import Login3 from './Login3';
+
+function App() {
+  return (
+    // 
+    <div>
+      <Login/>
+      <Login2/>
+      <Login3/>
+    </div>
+  );
+}
+
+export default App;
